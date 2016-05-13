09ef0b493388b748f221926e6f0881daabb0b128
dawnstar64 <elena.ariza@menloschool.org>
2016-05-13 15:17:42 -0700

    changed some things in initializer

--- Conway Game of Life/Piece.swift
+++ Conway Game of Life/Piece.swift
@@ -12,64 +12,53 @@ import SpriteKit
 class Piece {
     var row: Int
     var column: Int
-    var attack: Double!
-    var defense: Double!
-    var range: Int!
-    var health: Double!
-    var movement: Int!
+    var attack: Double
+    var defense: Double
+    var range: Int
+    var health: Int
+    var movement: Int
     
-    var sprite: SKSpriteNode?
+    var sprite: SKSpriteNode!
     
-    init(rowIn: Int, columnIn: Int) {
-        row = rowIn
-        column = columnIn
+    init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+        self.row = row
+        self.column = column
+        self.attack = attack
+        self.defense = defense
+        self.range = range
+        self.health = health
+        self.movement = movement
     }
 }
 
 class Warrior: Piece {
-    override init(rowIn: Int, columnIn: Int) {
-        super.init(rowIn: rowIn, columnIn: columnIn)
-        attack = 3
-        defense = 2
-        range = 1
-        health = 10
-        movement = 1
+    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+        
+        super.init(row: row, column: column, attack: 3, defense: 2, range: 1, health: 10, movement: 1)
         sprite = SKSpriteNode(imageNamed: "sprite warrior v1")
     }
 }
 
 class Ranger: Piece {
-    override init(rowIn: Int, columnIn: Int) {
-        super.init(rowIn: rowIn, columnIn: columnIn)
-        attack = 2
-        defense = 2
-        range = 2
-        health = 8
-        movement = 1
+    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+        
+        super.init(row: row, column: column, attack: 2, defense: 2, range: 2, health: 8, movement: 1)
         sprite = SKSpriteNode(imageNamed: "sprite ranger v1")
     }
 }
 
 class Defender: Piece {
-    override init(rowIn: Int, columnIn: Int) {
-        super.init(rowIn: rowIn, columnIn: columnIn)
-        attack = 1
-        defense = 5
-        range = 1
-        health = 15
-        movement = 1
+    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+        
+        super.init(row: row, column: column, attack: 1, defense: 5, range: 1, health: 15, movement: 1)
         sprite = SKSpriteNode(imageNamed: "sprite defender v1")
     }
 }
 
 class Mage: Piece {
-    override init(rowIn: Int, columnIn: Int) {
-        super.init(rowIn: rowIn, columnIn: columnIn)
-        attack = 3
-        defense = 2
-        range = 3
-        health = 10
-        movement = 1
+    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+        
+        super.init(row: row, column: column, attack: 3, defense: 2, range: 3, health: 6, movement: 1)
         sprite = SKSpriteNode(imageNamed: "sprite mage v1")
     }
 }
