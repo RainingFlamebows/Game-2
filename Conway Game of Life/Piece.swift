56c749d0cfc43cb89a746e93a9485533e9a41b3c
RainingFlamebows <Shannon@Tabula-Rasa.local>
2016-05-18 22:04:09 -0700

    Added functionality to Piece class
    - heal function for Mage
    - owner property for piece class
    - isAlive property for piece class
    - attack function for piece class
    - made sprites different for different owners

--- Conway Game of Life/Piece.swift
+++ Conway Game of Life/Piece.swift
@@ -12,15 +12,20 @@ import SpriteKit
 class Piece {
     var row: Int
     var column: Int
-    var attack: Double
-    var defense: Double
+    var attack: Int
+    var defense: Double // <- do we still need defense?
     var range: Int
     var health: Int
+    var currentHealth: Int
     var movement: Int
+    
+    var isAlive: Bool
+    var owner: Int // player 1 = 1, player 2 = 2
     
     var sprite: SKSpriteNode!
     
-    init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+    init(owner: Int, row: Int, column: Int, attack: Int, defense: Double, range: Int, health: Int, movement: Int) {
+        self.owner = owner
         self.row = row
         self.column = column
         self.attack = attack
@@ -28,38 +33,98 @@ class Piece {
         self.range = range
         self.health = health
         self.movement = movement
+        self.isAlive = true
+        
+        currentHealth = self.health // piece has max HP by default
+    }
+    
+    func attack(target: Piece) {
+        let newHealth = target.currentHealth - self.attack
+        if(newHealth < 0) {
+            target.isAlive = false
+        }
+        else {
+            target.currentHealth = newHealth
+        }
+    }
+    
+    // gives a list of available cells piece can move into while 
+    // taking into account the location of other pieces and objects
+    // then returns a list of potential places piece can move to
+    // and potential places piece can attack
+    func availableMoves() {
+        var moves = [()]
+        var attacks = [()]
+        for row in column-range...column+range {
+            for col in row-range...row+range {
+                // check if cell exists at that row, col
+                // check if there's any objects at that location
+                // if there is a piece at that location
+            }
+        }
     }
 }
 
 class Warrior: Piece {
-    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+    override init(owner: Int, row: Int, column: Int, attack: Int, defense: Double, range: Int, health: Int, movement: Int) {
         
-        super.init(row: row, column: column, attack: 3, defense: 2, range: 1, health: 10, movement: 1)
-        sprite = SKSpriteNode(imageNamed: "sprite warrior v1")
+        super.init(owner: owner, row: row, column: column, attack: 3, defense: 2, range: 1, health: 10, movement: 1)
+        if(owner == 1) {
+            sprite = SKSpriteNode(imageNamed: "warrior sprite red")
+        }
+        else if(owner == 2) {
+            sprite = SKSpriteNode(imageNamed: "warrior sprite blue")
+        }
     }
 }
 
 class Ranger: Piece {
-    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+    override init(owner: Int, row: Int, column: Int, attack: Int, defense: Double, range: Int, health: Int, movement: Int) {
         
-        super.init(row: row, column: column, attack: 2, defense: 2, range: 2, health: 8, movement: 1)
-        sprite = SKSpriteNode(imageNamed: "sprite ranger v1")
+        super.init(owner: owner, row: row, column: column, attack: 2, defense: 2, range: 2, health: 8, movement: 1)
+        if(owner == 1) {
+            sprite = SKSpriteNode(imageNamed: "ranger sprite red")
+        }
+        else if(owner == 2) {
+            sprite = SKSpriteNode(imageNamed: "ranger sprite blue")
+        }
     }
 }
 
 class Defender: Piece {
-    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+    override init(owner: Int, row: Int, column: Int, attack: Int, defense: Double, range: Int, health: Int, movement: Int) {
         
-        super.init(row: row, column: column, attack: 1, defense: 5, range: 1, health: 15, movement: 1)
-        sprite = SKSpriteNode(imageNamed: "sprite defender v1")
+        super.init(owner: owner, row: row, column: column, attack: 1, defense: 5, range: 1, health: 15, movement: 1)
+        if(owner == 1) {
+            sprite = SKSpriteNode(imageNamed: "defender sprite red")
+        }
+        else if(owner == 2) {
+            sprite = SKSpriteNode(imageNamed: "defender sprite blue")
+        }
     }
 }
 
 class Mage: Piece {
-    override init(row: Int, column: Int, attack: Double, defense: Double, range: Int, health: Int, movement: Int) {
+    override init(owner: Int, row: Int, column: Int, attack: Int, defense: Double, range: Int, health: Int, movement: Int) {
         
-        super.init(row: row, column: column, attack: 3, defense: 2, range: 3, health: 6, movement: 1)
-        sprite = SKSpriteNode(imageNamed: "sprite mage v1")
+        super.init(owner: owner, row: row, column: column, attack: 3, defense: 2, range: 3, health: 6, movement: 1)
+        if(owner == 1) {
+            sprite = SKSpriteNode(imageNamed: "mage sprite red")
+        }
+        else if(owner == 2) {
+            sprite = SKSpriteNode(imageNamed: "mage sprite blue")
+        }
+    }
+    
+    // heals the target piece (arbitrarily) by 50% of its health
+    func heal(target: Piece) {
+        let newHealth = target.currentHealth + target.health/2
+        if(newHealth > target.health) {
+            target.currentHealth = target.health
+        }
+        else {
+            target.currentHealth = newHealth
+        }
     }
 }
 
