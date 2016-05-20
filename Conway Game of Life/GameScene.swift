ffbbe7a47511e30c5ef4114740799e0f89d4f4ef
dawnstar64 <elena.ariza@menloschool.org>
2016-05-20 13:58:00 -0700

    attempted some more constraints

--- Conway Game of Life/GameScene.swift
+++ Conway Game of Life/GameScene.swift
@@ -147,18 +147,30 @@ class GameScene: SKScene {
         if (camera?.xScale >= 0.25 && sender.scale > previousScale) ||
             (camera?.xScale <= 1 && sender.scale < previousScale) {
             
-            previousScale = sender.scale
+            if camera?.xScale > 1 {
+                camera?.setScale(1)
+            }
+            else if camera?.xScale < 0.25 {
+                camera?.setScale(0.25)
+            }
+            else {
+                camera!.setScale(newScale)
+            }
             
             let locationInView = sender.locationInView(self.view)
             let location = self.convertPointFromView(locationInView)
             
-            camera!.setScale(newScale)
-            
+            if camera?.xScale == 1 {
+//                sender.scale = 1.0
+                camera?.position = CGPoint(x: frame.midX, y: frame.midY)
+            }
+            else {
                 let locationAfterScale = self.convertPointFromView(locationInView)
                 let locationDelta = CGPoint(x: location.x - locationAfterScale.x, y: location.y - locationAfterScale.y)
                 let newPoint = CGPoint(x: camera!.position.x - locationDelta.x, y: camera!.position.y - locationDelta.y)
                 camera!.position = newPoint
-                sender.scale = 1.0
+
+            }
         }
     }
    
